11ec1c353fe810399b6ff95a94a93152b677faaf
sahana-h <sahanahariharan@gmail.com>
2022-07-19 11:52:30 -0700

    Update sender.ino

--- sender.ino
+++ sender.ino
@@ -1,15 +1,10 @@
-/*********
-  Modified from the examples of the Arduino LoRa library
-  More resources: https://randomnerdtutorials.com
-*********/
-
 #include <SPI.h>
 #include <LoRa.h>
 
 //define the pins used by the transceiver module
-#define ss 5
+#define ss 18
 #define rst 14
-#define dio0 2
+#define dio0 26
 
 int counter = 0;
 
@@ -21,13 +16,13 @@ void setup() {
 
   //setup LoRa transceiver module
   LoRa.setPins(ss, rst, dio0);
-  
-  //replace the LoRa.begin(---E-) argument with your location's frequency 
+ 
+  //replace the LoRa.begin(---E-) argument with your location's frequency
   //433E6 for Asia
   //866E6 for Europe
   //915E6 for North America
   while (!LoRa.begin(915E6)) {
-    Serial.println(".");
+    Serial.println("...");
     delay(500);
   }
    // Change sync word (0xF3) to match the receiver
@@ -39,15 +34,14 @@ void setup() {
 
 void loop() {
   Serial.print("Sending packet: ");
-  Serial.println(counter);
+  Serial.println(String(counter)+" isabelleeeeeeeee");
 
   //Send LoRa packet to receiver
   LoRa.beginPacket();
-  LoRa.print("hello ");
-  LoRa.print(counter);
+  LoRa.print(String(counter)+"isabelleeeee");
   LoRa.endPacket();
 
   counter++;
 
-  delay(10000);
+  delay(5000);
 }
